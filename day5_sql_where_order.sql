f0d37054b5545eb46f37c297e56ef9f40fa26c8b
nithishkumarchinu <rnithishkumarchinu@gmail.com>
2026-01-02 20:51:24 +0530

    Added day 5 SQL WHERE and ORDER BY queries

---
+++ day5_sql_where_order.sql
@@ -0,0 +1,9 @@
+-- Day 5: SQL WHERE and ORDER BY
+
+SELECT * FROM students WHERE age > 20;
+
+SELECT name, department FROM students WHERE department = 'CSE';
+
+SELECT * FROM students ORDER BY age ASC;
+
+SELECT * FROM students ORDER BY name DESC;
